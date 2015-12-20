9b8b3414488d0671cd49c64ac3319e6ed8042f68
arve0 <arve.seljebu@gmail.com>
2015-12-20 18:24:07 +0100

    ghpages: fix master commit

--- ghpages.sh
+++ ghpages.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 # publish dist to gh-pages
 
+# store stash list
+stash_list=$(git stash list)
+
 npm run build &&
-git add --all . &&
-git commit -m "update dist build" &&
 cp -r dist .. &&  # move to temp location
+git add --all . &&
+git stash &&
 git checkout gh-pages &&
 cp -r ../dist/* . &&
 rm -r ../dist &&
@@ -12,3 +15,11 @@ git add --all . &&
 git amend -m "update gh-pages" &&  # do not keep history
 git push --force &&
 git checkout master
+
+# stash list is not equal -> pop
+new_stash_list=$(git stash list)
+if [ "$stash_list" != "$new_stash_list" ]
+then
+  echo "popping stash"
+  git stash pop
+fi
